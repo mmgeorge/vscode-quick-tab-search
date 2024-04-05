b334464da958eeed11bd1eba8512bb852cea1b46
Matt George <mattmg412@gmail.com>
2024-04-04 21:26:36 -0700

    Add sorting by type

--- src/QuickTabPicker.ts
+++ src/QuickTabPicker.ts
@@ -1,7 +1,7 @@
 import { homedir } from "os";
 import path from "path";
 import { runInThisContext } from "vm";
-import { QuickInputButton, QuickPickItem, QuickPickItemKind, Tab, TabInputCustom, TabInputNotebook, TabInputNotebookDiff, TabInputTerminal, TabInputText, TabInputTextDiff, TabInputWebview, ThemeIcon, TreeItem, Uri, window, workspace } from "vscode";
+import { QuickInputButton, QuickPickItem, QuickPickItemKind, Tab, TabInputCustom, TabInputNotebook, TabInputNotebookDiff, TabInputTerminal, TabInputText, TabInputTextDiff, TabInputWebview, ThemeColor, ThemeIcon, TreeItem, Uri, window, workspace } from "vscode";
 
 type TabInputType = TabInputText | TabInputTextDiff | TabInputCustom | TabInputWebview | TabInputNotebook | TabInputNotebookDiff | TabInputTerminal;
 
@@ -20,15 +20,19 @@ interface MagitTabInput {
   inner: TabInputText;
 }
 
-type TabInputUnion = FileTabInput | GitDiffTabInput | MagitTabInput;
+interface InputTypeOther {
+  type: "other";
+}
+
+type TabInputUnion = FileTabInput | GitDiffTabInput | MagitTabInput | InputTypeOther;
 
 class QuickTabItem implements QuickPickItem {
   constructor(private readonly _tab: Tab) {
     this._input = this._initInput();
   }
 
-  private _input: TabInputUnion | null = null;
-  private _initInput(): TabInputUnion | null {
+  private _input: TabInputUnion;
+  private _initInput(): TabInputUnion {
     const input = this._tab.input as any as TabInputType;
 
     if (input) {
@@ -47,11 +51,46 @@ class QuickTabItem implements QuickPickItem {
       }
     }
 
-    return null;
+    return { type: "other" };
+  }
+
+  get isActive(): Tab["isActive"] {
+    return this._tab.isActive;
+  }
+
+  get isPinned(): Tab["isPinned"] {
+    return this._tab.isPinned;
+  }
+
+  get isDirty(): Tab["isDirty"] {
+    return this._tab.isDirty;
+  }
+
+  get type(): TabInputUnion["type"] {
+    return this._input.type;
   }
 
   get label(): string {
+    const icon = this.isDirty ? "$(close-dirty) " : "      ";
+    return `${icon}${this._tab.label}`;
+  }
+
+  get ident(): string {
     return this._tab.label;
+  }
+
+  get buttons(): QuickInputButton[] {
+    if (this.isDirty) {
+      // Color never changes if we do this
+      // 
+      // return [
+      //   {
+      //     tooltip: "Buffer has unsaved changes",
+      //     iconPath: new ThemeIcon("close-dirty", new ThemeColor("button.hoverBackground")),
+      //   }
+      // ];
+    }
+    return [];
   }
 
   get description(): string | undefined {
@@ -90,20 +129,64 @@ class QuickTabItem implements QuickPickItem {
   }
 }
 
+let lastActiveTabIdent: string | null = null;
+
 export class QuickTabPicker {
   readonly _inner = window.createQuickPick<QuickTabItem>();
   readonly _items = new Array<QuickTabItem>();
 
   constructor() {
     const tabGroups = window.tabGroups.all;
+    const files: QuickPickItem[] = [];
+    const diffs: QuickPickItem[] = [];
+    const other: QuickPickItem[] = [];
+    let mostRecent: QuickTabItem | null = null;
 
     for (const group of tabGroups) {
       for (const tab of group.tabs) {
-        this._items.push(new QuickTabItem(tab));
+        const item = new QuickTabItem(tab);
+
+        if (item.ident === lastActiveTabIdent) {
+          mostRecent = item;
+        } else if (item.type === "file") {
+          files.push(item);
+        } else if (item.type === "git") {
+          diffs.push(item);
+        } else {
+          other.push(item);;
+        }
       }
     }
 
-    this._inner.items = this._items;
+    const sorted: QuickPickItem[] = [];
+
+    if (mostRecent) {
+      sorted.push({
+        label: "Recent",
+        kind: QuickPickItemKind.Separator
+      });
+      sorted.push(mostRecent);
+    }
+
+    sorted.push({
+      label: "Text",
+      kind: QuickPickItemKind.Separator
+    });
+    sorted.push(...files);
+
+    sorted.push({
+      label: "Git",
+      kind: QuickPickItemKind.Separator
+    });
+    sorted.push(...diffs);
+
+    sorted.push({
+      label: "System",
+      kind: QuickPickItemKind.Separator
+    });
+    sorted.push(...other);
+
+    this._inner.items = sorted as any;
     this._inner.show();
   }
 
